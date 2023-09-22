a7fd88d57c6fc8f191110d16fd396834035fe856
David Nichols <dnicho26@uncc.edu>
2023-09-22 15:00:59 -0400

    Update simulator.h
    working queue but not stall

--- HW2/simulator.h
+++ HW2/simulator.h
@@ -8,24 +8,72 @@
 class Simulator {
 private:
     std::queue<Event> events;  // Queue to store the events
+    std::queue<InstructionSet> instructionQueue; // Instruction queue
     int x1 = 160;  // Register x1 initialized to 160
     int x2 = 0;  // Register x2 initialized to 0
     float f0 = 0.0, f2 = 0.0, f4 = 0.0;  // Registers for floating point operations
-    int fetch = 1, decode = 0, execute = 0, store = 0;
+    int fetch = NO_OP, decode = NO_OP, execute = NO_OP, store = NO_OP;
 
 public:
+   Simulator() {
+        // Initial fill of the instruction queue
+        loadInstructions();
+    }
+
+    void loadInstructions() {
+	instructionQueue.push(ld);
+	instructionQueue.push(add);
+	instructionQueue.push(sd);
+	instructionQueue.push(addi);
+	instructionQueue.push(bne);
+
+    }
+    
     // Function to simulate the pipeline
     void simulatePipeline() {
         int clock_cycle = 1;
-        events.push(Event(fetch, NO_OP,  NO_OP,  NO_OP, x1, clock_cycle++));
+       // events.push(Event(fetch, NO_OP,  NO_OP,  NO_OP, x1, clock_cycle++));
+
         while (x1 != x2) {
         //************************************************************************************************************
+        
+              
+            if(instructionQueue.empty()) {
+                loadInstructions();  // Re-load instructions for next iteration
+            }
+            
+                        
+        if(execute == add && instructionQueue.front() == sd) {
+                instructionQueue.push(STALL);  // Inject a stall after add
+            }
+            
+        // Reposition 'addi' if 'ld' is in 'store' and 'addi' is next in line
+        if(store == ld && instructionQueue.front() == addi) {
+            instructionQueue.push(addi);  // push 'addi' to the back
+            instructionQueue.pop();       // remove the front 'addi'
+        }
+        
+	// Check if 'addi' is available during stall and move it up
+        if(fetch == STALL && !instructionQueue.empty() && instructionQueue.front() == addi) {
+            fetch = addi;
+            instructionQueue.pop();
+        } 
+        
+        else if(fetch == STALL && !instructionQueue.empty()) {
+            fetch = instructionQueue.front();
+            instructionQueue.pop();
+        }
+       
             if(decode == NO_OP && execute == NO_OP && store == NO_OP) {
                 decode = fetch;
-                fetch++;
-                if (fetch > bne)
-                    fetch = ld;
-            } else if(decode != NO_OP) {
+		fetch = instructionQueue.front();
+                instructionQueue.pop();
+                
+           // if (fetch > bne)
+           //         fetch = ld;
+            }
+            
+             else if(decode != NO_OP) {
                 execute = decode;
                 decode = NO_OP;
             } else if(execute != NO_OP) {
